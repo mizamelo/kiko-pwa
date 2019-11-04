3f19193a50481d7602d3055f27090e856110363d
Your Name <you@example.com>
2019-11-04 17:35:40 -0300

    created btn styles

--- src/page/Login/index.js
+++ src/page/Login/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Input from '../../components/components/InputComponent';
+import './styles.scss';
 
 // import { Container } from './styles';
 
 export default function Login() {
   return (
     <div className="container">
-      <Input name={"name"} title={"Nome"}/>
-      <Input name={"name"} title={"E-mail"}/>
+      <form className="login-position">
+        <Input type={"text"} name={"name"} title={"E-mail"}/>
+        <Input type={"password"} name={"name"} title={"Password"}/>
+        <button className="btn-default" type="submit">Solicitar</button>
+      </form>
     </div>
   );
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -2,11 +2,14 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+  @import url('https://fonts.googleapis.com/css?family=Poppins&display=swap');
+  
   * {
     padding: 0;
     margin: 0;
     outline: 0;
     box-sizing: border-box;
+    font-family: Poppins !important;
   }
   body {
     background: #2E5077;
@@ -29,5 +32,25 @@ export default createGlobalStyle`
     max-width: 320px;
     width: 320px;
     margin: 0 auto; 
+  }
+
+  .btn-default {
+    width: 292px;
+    margin: 0 auto;
+    height: 47px;
+    border-radius: 10px;
+    border: none;
+    background: #F6C543;
+    box-shadow: 0px 4px 13px rgba(0, 0, 0 0.25);
+    font-size:15px;
+    font-weight: 600;
+    font-family: Poppins, Sans Serif;
+    font-style: normal;
+    color: #2E5077;
+
+    &:hover {
+      box-shadow: 0px 4px 13px rgba(0, 0, 0 0.45);
+      background: #FADC8C;
+    }
   }
 `;
